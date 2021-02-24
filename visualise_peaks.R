e4463a00f59d0f64d739d6e60410a32c2704d440
Clodagh <clodagh.mcguire@nhs.net>
2021-02-24 15:02:25 +0000

    save peak visualisation to png

--- visualise_peaks.R
+++ visualise_peaks.R
@@ -9,20 +9,23 @@ gene_list = read.delim("hglft_genome_3d617_4fe3c0.bed")
 for (row in 1:nrow(gene_list)) { 
   gene_name <- gene_list[row, "name"]
   chr <- gene_list[row, "chr"]
-  start_pos <- gene_list[row, "start"] - 1000
-  end_pos <- gene_list[row, "end"] + 1000
+  start_pos <- gene_list[row, "start"] - 10000
+  end_pos <- gene_list[row, "end"] + 10000
+  
+  filename <- paste("./genes/",gene_name,".png", sep ="")
+  png(file=filename)
   
   gr <- GRanges(chr, IRanges(start_pos, end_pos))
-  trs <- geneModelFromTxdb(TxDb.Hsapiens.UCSC.hg18.knownGene, org.Hs.eg.db, gr=gr)
+  ids <- getGeneIDsFromTxDb(gr, TxDb.Hsapiens.UCSC.hg18.knownGene)
   
-  entrezID <- get(gene_name, org.Hs.egSYMBOL2EG)
-  theTrack <-geneTrack(entrezID, TxDb.Hsapiens.UCSC.hg18.knownGene, type="gene")[[1]]
+  symbols <- mget(ids, org.Hs.egSYMBOL)
+  genes <-geneTrack(ids, TxDb.Hsapiens.UCSC.hg18.knownGene, symbols, asList=FALSE)
   
-  viewerStyle <- trackViewerStyle()
-  setTrackStyleParam(Adult, "color", c("green", "black"))
-  setTrackStyleParam(Fetal, "color", c("blue", "black"))
-  setTrackViewerStyleParam(viewerStyle, "margin", c(.1, .05, .02, .02))
-  vp <- viewTracks(trackList(Adult, Fetal, trs), gr=gr, viewerStyle=viewerStyle, autoOptimizeStyle=TRUE)
+  optSty <- optimizeStyle(trackList(Adult, Fetal, genes), theme="safe")
+  trackListW <- optSty$tracks
+  viewerStyleW <- optSty$style
+  viewTracks(trackListW, gr=gr, viewerStyle=viewerStyleW)
+  dev.off()
 
   }
 
