39eead6f235eb68f50430554e5a12a04a61a1f05
Clodagh <clodagh.mcguire@nhs.net>
2021-02-25 13:58:12 +0000

    only visualise peaks in RP genes annotated by ChIPseeker

--- visualise_peaks.R
+++ visualise_peaks.R
@@ -4,6 +4,8 @@ library(org.Hs.eg.db)
 Adult <- importScore('GSM970258_GATA1-A.wig.gz', format='WIG')
 Fetal <- importScore('GSM970257_GATA1-F.wig.gz', format='WIG')
 
+adult_RP_genes <- read.csv("RPgenePeaks_GSM970258_A")
+fetal_RP_genes <- read.csv("RPgenePeaks_GSM970257_F")
 gene_list = read.delim("hglft_genome_3d617_4fe3c0.bed")
 
 for (row in 1:nrow(gene_list)) { 
@@ -12,20 +14,22 @@ for (row in 1:nrow(gene_list)) {
   start_pos <- gene_list[row, "start"] - 10000
   end_pos <- gene_list[row, "end"] + 10000
   
-  filename <- paste("./genes/",gene_name,".png", sep ="")
-  png(file=filename)
-  
-  gr <- GRanges(chr, IRanges(start_pos, end_pos))
-  ids <- getGeneIDsFromTxDb(gr, TxDb.Hsapiens.UCSC.hg18.knownGene)
-  
-  symbols <- mget(ids, org.Hs.egSYMBOL)
-  genes <-geneTrack(ids, TxDb.Hsapiens.UCSC.hg18.knownGene, symbols, asList=FALSE)
-  
-  optSty <- optimizeStyle(trackList(Adult, Fetal, genes), theme="safe")
-  trackListW <- optSty$tracks
-  viewerStyleW <- optSty$style
-  viewTracks(trackListW, gr=gr, viewerStyle=viewerStyleW)
-  dev.off()
+  if(gene_name %in% adult_RP_genes$SYMBOL||gene_name %in% fetal_RP_genes$SYMBOL){
+    filename <- paste("./genes/",gene_name,".png", sep ="")
+    png(file=filename)
+    
+    gr <- GRanges(chr, IRanges(start_pos, end_pos))
+    ids <- getGeneIDsFromTxDb(gr, TxDb.Hsapiens.UCSC.hg18.knownGene)
+    
+    symbols <- mget(ids, org.Hs.egSYMBOL)
+    genes <-geneTrack(ids, TxDb.Hsapiens.UCSC.hg18.knownGene, symbols, asList=FALSE)
+    
+    optSty <- optimizeStyle(trackList(Adult, Fetal, genes), theme="safe")
+    trackListW <- optSty$tracks
+    viewerStyleW <- optSty$style
+    viewTracks(trackListW, gr=gr, viewerStyle=viewerStyleW)
+    dev.off()
+  }
 
   }
 
