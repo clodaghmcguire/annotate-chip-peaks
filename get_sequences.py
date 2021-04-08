b73166eaab54cdf787a209beeadd8c079775726b
Clodagh <clodagh.mcguire@nhs.net>
2021-04-08 16:41:32 +0100

    save peak sequences in fasta format

--- get_sequences.py
+++ get_sequences.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 Entrez.email = "clodagh.mcguire@nhs.net"
 chr_accession = pd.read_csv('chr_accession_no.csv')
-peaks = pd.read_csv('RPgenePeaks_all_adult_peaks_hg19.csv')
+peaks = pd.read_csv('assigned_peaks/RPgenePeaks_all_adult_peaks_hg19.csv')
 peaks = pd.merge(peaks, chr_accession, on="chr")
 peaks["sequence"] = ""
+peaks_list = []
 
 for i in range(len(peaks)):
 
@@ -18,5 +19,11 @@ for i in range(len(peaks)):
     record = SeqIO.read(handle, "fasta")
     handle.close()
     peaks.loc[i, "sequence"] = record.seq
+    header = "\n>" + str(peaks.loc[i, "chr"]) + ":" + str(peaks.loc[i, "start"]) + "-" + str(peaks.loc[i, "end"])
+    peaks_list.append(header)
+    peaks_list.append(record.seq)
 
 peaks.to_csv('peak_sequences.txt', sep='\t')
+with open('peaks.fasta', 'w') as f:
+    for item in peaks_list:
+        f.write("%s\n" % item)
